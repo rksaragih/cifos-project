525954e7c368a138bc8f40772870b5cff5b8c090
cahyailhamketua <cahyailham811@gmail.com>
2025-10-28 13:37:56 +0700

    crud category

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AdminController;
 use App\Http\Controllers\Api\MenuController;
+use App\Http\Controllers\Api\CategoryController;
 
 // Admin login route
 Route::post('/admin/login', [AdminController::class, 'login']);
@@ -17,4 +18,15 @@ Route::middleware(['auth:sanctum', 'admin'])->group(function () {
     Route::post('/menus', [MenuController::class, 'store']);
     Route::put('/menus/{menu}', [MenuController::class, 'update']);
     Route::delete('/menus/{menu}', [MenuController::class, 'destroy']);
+});
+
+// Publicly accessible routes for Category
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{category}', [CategoryController::class, 'show']);
+
+// Admin-only routes for Category
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::put('/categories/{category}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
 });
