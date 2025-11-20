add39bfdf4fe718772c72baa1646aef5a3d0716f
rksaragih <168797432+rksaragih@users.noreply.github.com>
2025-11-20 10:43:17 +0700

    config WA

--- config/whatsapp.php
+++ config/whatsapp.php
@@ -2,6 +2,6 @@
 return [
     'base_url' => env('WHATSAPP_BASE_URL', 'http://localhost:3000'),
     'session' => env('WHATSAPP_SESSION', 'default'),
-    'api_key' => env('WHATSAPP_API_KEY', 'cfs_wa_api_7d9f2a1b4e8c'),
+    'api_key' => env('WHATSAPP_API_KEY', 'cfs_wa_api_8252f633c482868c73adc718e4896f06d19de7f467cd39d9d11d3577e69af1c1'),
     'admin_phone' => env('ADMIN_WHATSAPP', '62821647792927'),
 ];
