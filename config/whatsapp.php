6b79ac5261ac5b55d6503f69d4bfb4b23e401caa
rksaragih <168797432+rksaragih@users.noreply.github.com>
2025-11-20 11:31:44 +0700

    Ketinggalan Lagi

--- config/whatsapp.php
+++ config/whatsapp.php
@@ -3,5 +3,5 @@ return [
     'base_url' => env('WHATSAPP_BASE_URL', 'http://localhost:3000'),
     'session' => env('WHATSAPP_SESSION', 'default'),
     'api_key' => env('WHATSAPP_API_KEY', 'fake_api_key'),
-    'admin_phone' => env('ADMIN_WHATSAPP', '62821647792927'),
+    'admin_phone' => env('ADMIN_WHATSAPP', '628123456789'),
 ];
